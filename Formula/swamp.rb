11e19be24cc9805fa1534d21dbf0ca4af6b491df
Matthias Splieth <matthias@splieth.com>
2020-01-08 09:05:10 +0100

    Update version

--- Formula/swamp.rb
+++ Formula/swamp.rb
@@ -1,9 +1,9 @@
 class Swamp < Formula
   desc "Teh AWS profile manager"
   homepage "https://github.com/felixb/swamp"
-  url "https://github.com/felixb/swamp/archive/v0.10.0.tar.gz"
-  version "0.10.0"
-  sha256 "4cdcdd3e7b9085de5c0f53a9783e3392680727372d89f0e38270ab8da889b1d8"
+  url "https://github.com/felixb/swamp/archive/v0.11.0.tar.gz"
+  version "0.11.0"
+  sha256 "58c517f21160e19015d98a758a94bf8ebfc390cf1b3532c360cc890378ad5163"
   head "https://github.com/felixb/swamp.git"
 
   depends_on "go" => :build
@@ -15,8 +15,7 @@ class Swamp < Formula
     bin_path.install Dir["*"]
 
     cd bin_path do
-      system "make", "swamp_darwin"
-      mv "swamp_darwin", "swamp"
+      system "go", "build", "-o", "swamp"
       bin.install "swamp"
     end
   end
